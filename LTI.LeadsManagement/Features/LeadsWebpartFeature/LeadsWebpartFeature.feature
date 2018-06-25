3ae97e19e68f01c60e59b09ddf7e30ee311fcd49
siddharth <siddharthmukundan@gmail.com>
2018-06-25 11:12:35 +0530

    client object model csv added

---
+++ LTI.LeadsManagement/Features/LeadsWebpartFeature/LeadsWebpartFeature.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="c7db5013-dea7-402a-aa07-dfdd860ac00a" featureId="c7db5013-dea7-402a-aa07-dfdd860ac00a" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="LeadsWebpartFeature" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="53b23228-9c1d-42ff-8697-410ed5c60335" />
+  </projectItems>
+</feature>
---
+++ LeadsCSV/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+using System.IO;
+using System.Net;
+
+
+
+namespace LeadsCSV
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            SPSite site = new SPSite("http://leadsmgmt.trg14.int/");
+            SPWeb web = site.RootWeb;
+
+
+
+            SPList list = web.Lists["CompanyList"];
+
+
+
+
+
+            using (var reader = new StreamReader(@"c:\CompanyListCSV.csv"))
+
+            {
+
+                while (!reader.EndOfStream)
+
+
+                {
+
+                    string line = reader.ReadLine();
+
+                    var cols = line.Split(',');
+
+
+
+                    SPListItem newItem = list.Items.Add();
+
+                    newItem["CompanyName"] = "lti";
+
+                    newItem["Category"] = "mnc";
+
+                    newItem["Address"] = "abc nagar";
+
+                    newItem["City"] = "chennai";
+
+                    newItem["PostalCode"] = "600082";
+
+                    newItem["State"] = "taminadu";
+
+                    newItem["Website"] = "http://www.facebook.com";
+
+                    newItem["Phone"] = "987654321";
+
+                    newItem["Fax Number"] = cols[8];
+
+                    newItem["Email"] = cols[9];
+
+                    newItem["Stage"] = cols[10];
+
+                    newItem["Notes"] = cols[11];
+
+
+
+                    newItem.Update();
+                }
+            }
+       }
+    }
+}
