22e7a1b92ae590b2ac646c38361db452858168a9
siddharth <siddharthmukundan@gmail.com>
2018-06-25 15:17:01 +0530

    Contact Webpart

--- companySVC/Program.cs
+++ companySVC/Program.cs
@@ -13,51 +13,67 @@ namespace LTI.LeadsManagement.Import
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("sales data being imported");
-            ClientContext ctx = new ClientContext("http://leadsmgmt.trg14.int/");
+            
+                Console.WriteLine("sales data being imported");
+           
+                ClientContext ctx = new ClientContext("http://leadsmgmt.trg14.int/");
+
             Console.WriteLine("check");//setting that the client is using it
-            using (var reader = new StreamReader(@"c:\CompanyListCSV.csv"))//the file to be imported
-            {
-                while (!reader.EndOfStream)
+           
+                using (var reader = new StreamReader(@"c:\company.csv"))//the file to be imported
                 {
-                    string line = reader.ReadLine();//reading the csv line
-                    var cols = line.Split(',');
-                    //Create SP lIst Item
-                    ListItemCreationInformation item = new ListItemCreationInformation();//creating an item
-                    ListItem newItem = ctx.Web.Lists.GetByTitle("CompanyList").AddItem(item);
 
-                    newItem["Title"] = cols[0];
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();//reading the csv line
+                        var cols = line.Split(',');
+                        //Create SP lIst Item
+                        ListItemCreationInformation item = new ListItemCreationInformation();//creating an item
+                        ListItem newItem = ctx.Web.Lists.GetByTitle("CompanyList").AddItem(item);
 
-
-                    newItem["category"] = cols[1];
-
-                    newItem["WorkAddress"] = cols[2];
-
-                    newItem["WorkCity"] = cols[3];
-
-                    newItem["PostalCode"] = cols[4];
-
-                    newItem["state"] = cols[5];
-
-                    newItem["Website"] = cols[6];
-
-                    newItem["Phone"] = cols[7];
-
-                    newItem["WorkFax"] = cols[8];
-
-                    newItem["Email"] = cols[9];
-
-                    newItem["Stage"] = cols[10];
+                        newItem["Title"] = cols[0];
 
 
+                        newItem["category"] = cols[1];
+
+                        newItem["WorkAddress"] = cols[2];
+
+                        newItem["WorkCity"] = cols[3];
+
+                        newItem["PostalCode"] = cols[4];
+
+                        newItem["state"] = cols[5];
+
+                        newItem["Website"] = cols[6];
+
+                        newItem["Phone"] = cols[7];
+
+                        newItem["WorkFax"] = cols[8];
+
+                        newItem["Email"] = cols[9];
+
+                        newItem["Stage"] = cols[10];
 
 
 
-                    newItem.Update();
-                    ctx.Load(newItem);
-                    Console.WriteLine("check");
-                    Console.WriteLine("hi");
-                    ctx.ExecuteQuery();
+
+
+                        newItem.Update();
+                        ctx.Load(newItem);
+
+                        ctx.ExecuteQuery();
+                    }
+
                 }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            Console.ReadLine();
+
+        }
     }
+
+
 }
