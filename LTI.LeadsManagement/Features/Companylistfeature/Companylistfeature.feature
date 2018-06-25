e529ce0551989dafc36e0c4c6053f510a77d12a9
siddharth <siddharthmukundan@gmail.com>
2018-06-25 14:03:09 +0530

    added csv import project

---
+++ LTI.LeadsManagement/Features/Companylistfeature/Companylistfeature.feature
@@ -0,0 +1,7 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="c5aaff9b-184e-4a24-a114-23da6bb1f383" featureId="c5aaff9b-184e-4a24-a114-23da6bb1f383" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Listcompany" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="4a0a3835-84f2-4e9b-9948-9ebce0ca069f" />
+    <projectItemReference itemId="a0e13921-aa18-466c-95c9-8fb5563b9276" />
+  </projectItems>
+</feature>
---
+++ companySVC/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using System.IO;
+using System.Net;
+
+namespace LTI.LeadsManagement.Import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sales data being imported");
+            ClientContext ctx = new ClientContext("http://leadsmgmt.trg14.int/");
+            Console.WriteLine("check");//setting that the client is using it
+            using (var reader = new StreamReader(@"c:\CompanyListCSV.csv"))//the file to be imported
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();//reading the csv line
+                    var cols = line.Split(',');
+                    //Create SP lIst Item
+                    ListItemCreationInformation item = new ListItemCreationInformation();//creating an item
+                    ListItem newItem = ctx.Web.Lists.GetByTitle("CompanyList").AddItem(item);
+
+                    newItem["Title"] = cols[0];
+
+
+                    newItem["category"] = cols[1];
+
+                    newItem["WorkAddress"] = cols[2];
+
+                    newItem["WorkCity"] = cols[3];
+
+                    newItem["PostalCode"] = cols[4];
+
+                    newItem["state"] = cols[5];
+
+                    newItem["Website"] = cols[6];
+
+                    newItem["Phone"] = cols[7];
+
+                    newItem["WorkFax"] = cols[8];
+
+                    newItem["Email"] = cols[9];
+
+                    newItem["Stage"] = cols[10];
+
+
+
+
+
+                    newItem.Update();
+                    ctx.Load(newItem);
+                    Console.WriteLine("check");
+                    Console.WriteLine("hi");
+                    ctx.ExecuteQuery();
+                }
+    }
+}
