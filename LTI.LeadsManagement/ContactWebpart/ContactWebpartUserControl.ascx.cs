54511e77bb794568cc289b37da48e53823e7977a
rapurusrinithya <rapuru.srinithya@gmail.com>
2018-06-26 14:58:50 +0530

    Added Model Classes

--- LTI.LeadsManagement/ContactWebpart/ContactWebpartUserControl.ascx.cs
+++ LTI.LeadsManagement/ContactWebpart/ContactWebpartUserControl.ascx.cs
@@ -2,13 +2,82 @@
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
+using System.Data;
+using Microsoft.SharePoint;
+using System.Web.Configuration;
 
 namespace LTI.LeadsManagement.ContactWebpart
 {
     public partial class ContactWebpartUserControl : UserControl
     {
+     
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            //if (!this.IsPostBack)
+            //{
+              
+            //    //DataTable items = this.getDetails();
+            //    //    this.dlContacts.DataSource = items;
+
+            //    //    this.dlContacts.DataBind();
+            //}
+        }
+        public DataTable GetDetails()
+        {
+
+            SPList Details = SPContext.Current.Web.Lists["ContactList"];
+
+            SPQuery q = new SPQuery();
+
+            q.Query = string.Format(@"<Where>
+  					  <Eq>
+       					  <FieldRef Name='GKH' />
+       					  <Value Type='Choice'>{0}</Value>
+    					  </Eq>
+  					 </Where>",ddlCompany.Text);
+            //SPListItemCollection results = Details.GetItems(q);
+            //Bind();
+            return Details.GetItems(q).GetDataTable();
+
+        }
+        public void Bind()
+        {
+            //try
+            //{
+                DataTable ds = this.GetDetails();
+                //results.Fill(ds, "Employee");
+                dlContacts.DataSource = ds;
+                dlContacts.DataBind();
+
+                //int index = e.Item.ItemIndex;
+                int count = 0;
+            try
+            {
+                //foreach (DataTable item in ds.)
+                while (count < ds.Rows.Count)
+                {
+                    //int index = results.Item.ItemIndex;
+                    Label Name = (Label)dlContacts.FindControl("lblName");
+                    Name.Text = ds.Rows[count]["Title"].ToString();
+                    Label Title = (Label)dlContacts.FindControl("lblTitle");
+                    Label Department = (Label)dlContacts.FindControl("lblDepartment");
+                    Label Email = (Label)dlContacts.FindControl("lblEmail");
+                    Label Phone = (Label)dlContacts.FindControl("lblPhone");
+                    Label Mobile = (Label)dlContacts.FindControl("lblMobile");
+                    FileUpload Image = (FileUpload)dlContacts.FindControl("Img");
+                    FileUpload LinkedIn = (FileUpload)dlContacts.FindControl("lblLink");
+                    FileUpload Facebook = (FileUpload)dlContacts.FindControl("lblFb");
+                    Label Twitter = (Label)dlContacts.FindControl("lblTwitter");
+                    Label Status = (Label)dlContacts.FindControl("lblStatus");
+                    count++;
+                }
+            }
+
+            catch
+            {
+
+            }
 
         }
 
@@ -20,6 +89,20 @@ namespace LTI.LeadsManagement.ContactWebpart
         protected void ddlCompany_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+
+        protected void btnSNotes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnShow_Click(object sender, EventArgs e)
+        {
+            //string CompanyName = this.ddlCompany.Text;
+
+            Bind();
+            
         }
     }
 }
---
+++ LTI.LeadsManagement/Model/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTI.LeadsManagement.Model
+{
+    public enum ContactStatus
+    {
+        ToContact = 1,
+        Contacted = 2,
+        Regular = 3
+    }
+
+}
